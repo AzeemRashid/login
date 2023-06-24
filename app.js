a2ab516f89e75f413e534fd8e035b8f9c32c7b84
Azeem Rashid <129035793+AzeemRashid@users.noreply.github.com>
2023-06-24 10:16:03 -0700

    its a just login form

---
+++ app.js
@@ -0,0 +1,17 @@
+ function sumbit(e){
+    var username=document.getElementById('Username').value
+    var password=document.getElementById('Password').value
+    e.value=""
+
+    var student={
+        username:username,
+        password:password
+
+    }
+    
+   
+    
+alert("welcome to:"+username); 
+// console.log("here is your password:":+student.password)
+ }
+ 
